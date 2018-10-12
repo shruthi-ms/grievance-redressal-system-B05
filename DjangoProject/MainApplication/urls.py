fcec39d2ad11fd46d2e91a87e84c49dc64a4fe19
shruthi-ms <shruthi.ms16@iiits.in>
2018-10-12 11:59:10 +0530

    Add files via upload
    added Main Application files

---
+++ DjangoProject/MainApplication/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^login$', views.Login,name='login'),
+    url(r'^category$', views.Category,name='category'),
+]
